d26d9c722784e31624ab0a1ff1e44a1d5ead25f1
Kate_Kalashnikova <68355941+KateKalashnikova@users.noreply.github.com>
2021-02-03 21:37:29 -0700

    updated Forms
    added multiply and subtract

---
+++ Form1.Designer.vb
@@ -0,0 +1,314 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtMessage = New System.Windows.Forms.Button()
+        Me.txtAddValue1 = New System.Windows.Forms.TextBox()
+        Me.btnAddValues = New System.Windows.Forms.Button()
+        Me.txtAddValue2 = New System.Windows.Forms.TextBox()
+        Me.lblAddEqual = New System.Windows.Forms.Label()
+        Me.txtSumAnswer = New System.Windows.Forms.TextBox()
+        Me.txtDivisionAnswer = New System.Windows.Forms.TextBox()
+        Me.lblDivideEqual = New System.Windows.Forms.Label()
+        Me.txtDivideValue2 = New System.Windows.Forms.TextBox()
+        Me.btnDivideValues = New System.Windows.Forms.Button()
+        Me.txtDivideValue1 = New System.Windows.Forms.TextBox()
+        Me.txtMultAnswer = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtMultValue2 = New System.Windows.Forms.TextBox()
+        Me.btnMultiplyValues = New System.Windows.Forms.Button()
+        Me.txtMultValue1 = New System.Windows.Forms.TextBox()
+        Me.txtSubstrAnswer = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSubstrValue2 = New System.Windows.Forms.TextBox()
+        Me.btnSubstr = New System.Windows.Forms.Button()
+        Me.txtSubstrValue1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(12, 20)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(49, 20)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(68, 20)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(208, 27)
+        Me.txtName.TabIndex = 1
+        Me.txtName.Text = "te"
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(13, 53)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 27)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "&OK"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(199, 53)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 27)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(107, 53)
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(75, 27)
+        Me.txtMessage.TabIndex = 4
+        Me.txtMessage.Text = "&Message"
+        Me.txtMessage.UseVisualStyleBackColor = True
+        '
+        'txtAddValue1
+        '
+        Me.txtAddValue1.Location = New System.Drawing.Point(13, 129)
+        Me.txtAddValue1.Name = "txtAddValue1"
+        Me.txtAddValue1.Size = New System.Drawing.Size(55, 27)
+        Me.txtAddValue1.TabIndex = 5
+        '
+        'btnAddValues
+        '
+        Me.btnAddValues.Location = New System.Drawing.Point(74, 129)
+        Me.btnAddValues.Name = "btnAddValues"
+        Me.btnAddValues.Size = New System.Drawing.Size(27, 27)
+        Me.btnAddValues.TabIndex = 6
+        Me.btnAddValues.Text = "+"
+        Me.btnAddValues.UseVisualStyleBackColor = True
+        '
+        'txtAddValue2
+        '
+        Me.txtAddValue2.Location = New System.Drawing.Point(107, 129)
+        Me.txtAddValue2.Name = "txtAddValue2"
+        Me.txtAddValue2.Size = New System.Drawing.Size(55, 27)
+        Me.txtAddValue2.TabIndex = 7
+        '
+        'lblAddEqual
+        '
+        Me.lblAddEqual.AutoSize = True
+        Me.lblAddEqual.Location = New System.Drawing.Point(174, 132)
+        Me.lblAddEqual.Name = "lblAddEqual"
+        Me.lblAddEqual.Size = New System.Drawing.Size(19, 20)
+        Me.lblAddEqual.TabIndex = 8
+        Me.lblAddEqual.Text = "="
+        '
+        'txtSumAnswer
+        '
+        Me.txtSumAnswer.Location = New System.Drawing.Point(199, 129)
+        Me.txtSumAnswer.Name = "txtSumAnswer"
+        Me.txtSumAnswer.Size = New System.Drawing.Size(55, 27)
+        Me.txtSumAnswer.TabIndex = 9
+        '
+        'txtDivisionAnswer
+        '
+        Me.txtDivisionAnswer.Location = New System.Drawing.Point(199, 174)
+        Me.txtDivisionAnswer.Name = "txtDivisionAnswer"
+        Me.txtDivisionAnswer.Size = New System.Drawing.Size(55, 27)
+        Me.txtDivisionAnswer.TabIndex = 14
+        '
+        'lblDivideEqual
+        '
+        Me.lblDivideEqual.AutoSize = True
+        Me.lblDivideEqual.Location = New System.Drawing.Point(174, 177)
+        Me.lblDivideEqual.Name = "lblDivideEqual"
+        Me.lblDivideEqual.Size = New System.Drawing.Size(19, 20)
+        Me.lblDivideEqual.TabIndex = 13
+        Me.lblDivideEqual.Text = "="
+        '
+        'txtDivideValue2
+        '
+        Me.txtDivideValue2.Location = New System.Drawing.Point(107, 174)
+        Me.txtDivideValue2.Name = "txtDivideValue2"
+        Me.txtDivideValue2.Size = New System.Drawing.Size(55, 27)
+        Me.txtDivideValue2.TabIndex = 12
+        '
+        'btnDivideValues
+        '
+        Me.btnDivideValues.Location = New System.Drawing.Point(74, 174)
+        Me.btnDivideValues.Name = "btnDivideValues"
+        Me.btnDivideValues.Size = New System.Drawing.Size(27, 27)
+        Me.btnDivideValues.TabIndex = 11
+        Me.btnDivideValues.Text = "/"
+        Me.btnDivideValues.UseVisualStyleBackColor = True
+        '
+        'txtDivideValue1
+        '
+        Me.txtDivideValue1.Location = New System.Drawing.Point(13, 174)
+        Me.txtDivideValue1.Name = "txtDivideValue1"
+        Me.txtDivideValue1.Size = New System.Drawing.Size(55, 27)
+        Me.txtDivideValue1.TabIndex = 10
+        '
+        'txtMultAnswer
+        '
+        Me.txtMultAnswer.Location = New System.Drawing.Point(199, 220)
+        Me.txtMultAnswer.Name = "txtMultAnswer"
+        Me.txtMultAnswer.Size = New System.Drawing.Size(55, 27)
+        Me.txtMultAnswer.TabIndex = 19
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(174, 223)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(19, 20)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "="
+        '
+        'txtMultValue2
+        '
+        Me.txtMultValue2.Location = New System.Drawing.Point(107, 220)
+        Me.txtMultValue2.Name = "txtMultValue2"
+        Me.txtMultValue2.Size = New System.Drawing.Size(55, 27)
+        Me.txtMultValue2.TabIndex = 17
+        '
+        'btnMultiplyValues
+        '
+        Me.btnMultiplyValues.Location = New System.Drawing.Point(74, 220)
+        Me.btnMultiplyValues.Name = "btnMultiplyValues"
+        Me.btnMultiplyValues.Size = New System.Drawing.Size(27, 27)
+        Me.btnMultiplyValues.TabIndex = 16
+        Me.btnMultiplyValues.Text = "*"
+        Me.btnMultiplyValues.UseVisualStyleBackColor = True
+        '
+        'txtMultValue1
+        '
+        Me.txtMultValue1.Location = New System.Drawing.Point(13, 220)
+        Me.txtMultValue1.Name = "txtMultValue1"
+        Me.txtMultValue1.Size = New System.Drawing.Size(55, 27)
+        Me.txtMultValue1.TabIndex = 15
+        '
+        'txtSubstrAnswer
+        '
+        Me.txtSubstrAnswer.Location = New System.Drawing.Point(199, 264)
+        Me.txtSubstrAnswer.Name = "txtSubstrAnswer"
+        Me.txtSubstrAnswer.Size = New System.Drawing.Size(55, 27)
+        Me.txtSubstrAnswer.TabIndex = 24
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(174, 267)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 20)
+        Me.Label2.TabIndex = 23
+        Me.Label2.Text = "="
+        '
+        'txtSubstrValue2
+        '
+        Me.txtSubstrValue2.Location = New System.Drawing.Point(107, 264)
+        Me.txtSubstrValue2.Name = "txtSubstrValue2"
+        Me.txtSubstrValue2.Size = New System.Drawing.Size(55, 27)
+        Me.txtSubstrValue2.TabIndex = 22
+        '
+        'btnSubstr
+        '
+        Me.btnSubstr.Location = New System.Drawing.Point(74, 264)
+        Me.btnSubstr.Name = "btnSubstr"
+        Me.btnSubstr.Size = New System.Drawing.Size(27, 27)
+        Me.btnSubstr.TabIndex = 21
+        Me.btnSubstr.Text = "-"
+        Me.btnSubstr.UseVisualStyleBackColor = True
+        '
+        'txtSubstrValue1
+        '
+        Me.txtSubstrValue1.Location = New System.Drawing.Point(13, 264)
+        Me.txtSubstrValue1.Name = "txtSubstrValue1"
+        Me.txtSubstrValue1.Size = New System.Drawing.Size(55, 27)
+        Me.txtSubstrValue1.TabIndex = 20
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtSubstrAnswer)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSubstrValue2)
+        Me.Controls.Add(Me.btnSubstr)
+        Me.Controls.Add(Me.txtSubstrValue1)
+        Me.Controls.Add(Me.txtMultAnswer)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMultValue2)
+        Me.Controls.Add(Me.btnMultiplyValues)
+        Me.Controls.Add(Me.txtMultValue1)
+        Me.Controls.Add(Me.txtDivisionAnswer)
+        Me.Controls.Add(Me.lblDivideEqual)
+        Me.Controls.Add(Me.txtDivideValue2)
+        Me.Controls.Add(Me.btnDivideValues)
+        Me.Controls.Add(Me.txtDivideValue1)
+        Me.Controls.Add(Me.txtSumAnswer)
+        Me.Controls.Add(Me.lblAddEqual)
+        Me.Controls.Add(Me.txtAddValue2)
+        Me.Controls.Add(Me.btnAddValues)
+        Me.Controls.Add(Me.txtAddValue1)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnOk As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtMessage As Button
+    Friend WithEvents txtAddValue1 As TextBox
+    Friend WithEvents btnAddValues As Button
+    Friend WithEvents txtAddValue2 As TextBox
+    Friend WithEvents lblAddEqual As Label
+    Friend WithEvents txtSumAnswer As TextBox
+    Friend WithEvents txtDivisionAnswer As TextBox
+    Friend WithEvents lblDivideEqual As Label
+    Friend WithEvents txtDivideValue2 As TextBox
+    Friend WithEvents btnDivideValues As Button
+    Friend WithEvents txtDivideValue1 As TextBox
+    Friend WithEvents txtMultAnswer As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtMultValue2 As TextBox
+    Friend WithEvents btnMultiplyValues As Button
+    Friend WithEvents txtMultValue1 As TextBox
+    Friend WithEvents txtSubstrAnswer As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSubstrValue2 As TextBox
+    Friend WithEvents btnSubstr As Button
+    Friend WithEvents txtSubstrValue1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        txtName.Text = "Hello " & txtName.Text
+    End Sub
+
+    Private Sub txtMessage_Click(sender As Object, e As EventArgs) Handles txtMessage.Click
+        MessageBox.Show("Nice to meet you " & txtName.Text,
+                        "Hello " & txtName.Text)
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnAddValues_Click(sender As Object, e As EventArgs) Handles btnAddValues.Click
+        Try
+            Dim firstNum As Integer = CInt(txtAddValue1.Text)
+            Dim secondNum As Integer = CInt(txtAddValue2.Text)
+            Dim sum = firstNum + secondNum
+
+            txtSumAnswer.Text = CStr(sum)
+
+        Catch ex As System.InvalidCastException
+
+            MessageBox.Show("Please enter numbers to sum",
+                               "Error")
+            Console.WriteLine("An error occured")
+
+        Catch ex As Exception
+
+            MessageBox.Show("An unknown error occured",
+                               "Error")
+
+        End Try
+    End Sub
+
+    Private Sub btnDivideValues_Click(sender As Object, e As EventArgs) Handles btnDivideValues.Click
+        Dim firstNum As Decimal = CDec(txtDivideValue1.Text)
+        Dim secondNum As Decimal = CDec(txtDivideValue2.Text)
+        Dim intSolution As Integer = CType(firstNum / secondNum, Integer)
+
+        txtDivisionAnswer.Text = CStr(intSolution)
+    End Sub
+
+    Private Sub btnMultiplyValues_Click(sender As Object, e As EventArgs) Handles btnMultiplyValues.Click
+        Dim firstNum As Decimal = CDec(txtMultValue1.Text)
+        Dim secondNum As Decimal = CDec(txtMultValue2.Text)
+        Dim intSolution As Integer = CType(firstNum * secondNum, Integer)
+
+        txtMultAnswer.Text = CStr(intSolution)
+    End Sub
+
+    Private Sub btnSubstr_Click(sender As Object, e As EventArgs) Handles btnSubstr.Click
+        Dim firstNum As Decimal = CDec(txtSubstrValue1.Text)
+        Dim secondNum As Decimal = CDec(txtSubstrValue2.Text)
+        Dim intSolution As Integer = CType(firstNum - secondNum, Integer)
+
+        txtSubstrAnswer.Text = CStr(intSolution)
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+
+    End Sub
+End Class
